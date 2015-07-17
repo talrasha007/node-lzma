502e2ec1182ebe9602b0fb018cdb4c4676e3392f
Tal <talrasha007@gmail.com>
2015-07-17 14:55:52 +0800

    LzmaEnc class.

--- src/module.cc
+++ src/module.cc
@@ -1,11 +1,102 @@
 #include <memory>
 #include <vector>
 #include <nan.h>
+#include <Alloc.h>
 #include <LzmaLib.h>
+#include <LzmaEnc.h>
 
 using namespace std;
 using namespace v8;
 using namespace node;
+
+class LzmaEnc : public node::ObjectWrap {
+public:
+    static void setup(Handle<Object>& exports) {
+        const char* className = "LzmaEnc";
+        
+        Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(LzmaEnc::ctor);
+        tpl->SetClassName(NanNew(className));
+        tpl->InstanceTemplate()->SetInternalFieldCount(1);
+        
+        NODE_SET_PROTOTYPE_METHOD(tpl, "push", LzmaEnc::push);
+        NODE_SET_PROTOTYPE_METHOD(tpl, "destroy", LzmaEnc::destroy);
+        
+        exports->Set(NanNew(className), tpl->GetFunction());
+    }
+    
+private:
+    static NAN_METHOD(ctor) {
+        NanScope();
+        
+        int level = 5;
+        int threads = 1;
+        
+        if (args[0]->IsNumber()) {
+            int argLvl = args[0]->Int32Value();
+            if (argLvl >= 0 && argLvl <= 9) level = argLvl;
+        }
+        
+        if (args[1]->IsNumber()) {
+            int argTh = args[1]->Int32Value();
+            if (argTh == 1 || argTh == 2) threads= argTh;
+        }
+        
+        LzmaEnc *ptr = new LzmaEnc(level, threads);
+        ptr->Wrap(args.This());
+        NanReturnValue(args.This());
+    }
+    
+    static NAN_METHOD(push) {
+        NanScope();
+        
+        size_t srcLen = Buffer::Length(args[0]);
+        char *src = Buffer::Data(args[0]);
+        
+        size_t dstLen = srcLen + 128 + srcLen / 3;
+        char *dst = new char[dstLen];
+        
+        LzmaEnc *ptr = ObjectWrap::Unwrap<LzmaEnc>(args.This());
+        LzmaEnc_MemEncode(ptr->_handle, (Byte*)dst, &dstLen, (Byte*)src, srcLen,
+                          0, NULL, &g_Alloc, &g_Alloc);
+        
+        
+        Local<Object> ret = NanNewBufferHandle(dstLen);
+        char *retBuf = Buffer::Data(ret);
+        memcpy(retBuf, dst, dstLen);
+        
+        delete dst;
+        NanReturnValue(ret);
+    }
+    
+    static NAN_METHOD(destroy) {
+        NanScope();
+        
+        LzmaEnc *ptr = ObjectWrap::Unwrap<LzmaEnc>(args.This());
+        LzmaEnc_Destroy(ptr->_handle, &g_Alloc, &g_Alloc);
+        ptr->_handle = NULL;
+        
+        NanReturnUndefined();
+    }
+    
+private:
+    LzmaEnc(int level, int threads) : _propBufSz(LZMA_PROPS_SIZE) {
+        LzmaEncProps_Init(&_props);
+        _props.level = level;
+        _props.numThreads = threads;
+        
+        _handle = LzmaEnc_Create(&g_Alloc);
+        
+        LzmaEnc_SetProps(_handle, &_props);
+        LzmaEnc_WriteProperties(_handle, _propBuf, &_propBufSz);
+    }
+    
+private:
+    CLzmaEncHandle _handle;
+    CLzmaEncProps _props;
+    
+    Byte _propBuf[LZMA_PROPS_SIZE];
+    size_t _propBufSz;
+};
 
 NAN_METHOD(compress) {
     NanScope();
@@ -47,6 +138,8 @@ NAN_METHOD(compress) {
 
 void init(v8::Handle<v8::Object> exports) {
     NODE_SET_METHOD(exports, "compress", compress);
+    
+    LzmaEnc::setup(exports);
 }
 
 NODE_MODULE(lzma, init);
